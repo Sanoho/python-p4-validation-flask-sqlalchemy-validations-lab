c3cf45151b400d289b7582028ce010e59b615227
Timothy <sanohoang@gmail.com>
2023-04-26 13:37:15 -0500

    completed validations lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,20 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        names = db.session.query(Author.name).all()
+        if not name:
+            raise ValueError('Author needs a name.')
+        elif name in names:
+            raise ValueError('Name must be unique.')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, number):
+        if len(number) != 10 :
+            raise ValueError('Phone number needs to be 10 digits long.')
+        return number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +41,28 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(word in title for word in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+    
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if key == 'content':
+            if len(string) <= 250:
+                raise ValueError('Content needs at least 250 characters.')
+        if key == 'summary':
+            if len(string) >= 250:
+                raise ValueError('Summary is too long, cannot be more than 250 characters.')
+        return string
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Category must be Fiction or Non-Fiction.')
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
